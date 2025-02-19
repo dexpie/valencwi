72f1624b49808b3ded093d443f44168a253ccdaa
dexpie <gadingputra103@gmail.com>
2025-02-19 11:31:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const wrapper = document.querySelector(".wrapper"); const question = document.querySelector(".question");
+const wrapper = document.querySelector(".wrapper"); 
+const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 const nextBtn = document.querySelector(".next-btn");
 
@@ -6,13 +7,11 @@ let nextClicked = false;
 
 nextBtn.addEventListener("click", () => {
     if (!nextClicked) {
-        question.innerHTML = "Thank you for loving
-        me, with all my shortcomings.";
-        gif.src= "https://raw.githubusercontent.com/anstrea/anstrea.github.io/main/Respon/Skibidi%201/thanks.gif";
-        nextBtn.innerHTML = "reply":
+        question.innerHTML = "Thank you for loving me, with all my shortcomings.";
+        gif.src = "https://raw.githubusercontent.com/anstrea/anstrea.github.io/main/Respon/Skibidi%201/thanks.gif";
+        nextBtn.innerHTML = "Reply";
         nextClicked = true;
-        
     } else {
-        window.location.href= "https://wa.me/?text=Terserah%20Mau%20Reply%20Apaaa";
-}
+        window.location.href = "https://wa.me/?text=Terserah%20Mau%20Reply%20Apaaa";
+    }
 });
